9cbe51f6e402d7d3d4694ca403031cd302e1e353
sun <743685264@qq,com>
2021-05-10 16:12:35 +0800

    加入创建数据库和表的各项配置.

---
+++ app/src/main/java/com/sun/coolweather/db/County.java
@@ -0,0 +1,42 @@
+package com.sun.coolweather.db;
+
+import org.litepal.crud.LitePalSupport;
+
+public class County extends LitePalSupport {
+    private int id;
+    private  String countryName ;
+    private String weatherId;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
